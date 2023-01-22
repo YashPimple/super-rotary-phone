9e16f3bdc412730ca68c3e27a85f6fa4b331189e
Vishv <vishvsalvi10@gmail.com>
2023-01-22 15:16:28 +0530

    merging the changes in backend as well as frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import {
+
+  
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
+import Calculate from "./pages/Calculate/Calculate";
+import Blogs from "./pages/Blogs/Blogs";
+import "./App.css"
+import Breakfast from "./pages/Calculate/Breakfast/Breakfast";
+import Error from "./pages/Error/Error";
+import Lunch from "./pages/Calculate/Lunch/Lunch";
+import Snacks from "./pages/Calculate/Snacks/Snacks";
+import Dinner from "./pages/Calculate/Dinner/Dinner";
+import SignUp from "./SignUp/SignUp";
+import SignIn from "./pages/SignIn/SignIn";
+import SharedLayout from "./pages/SharedLayout";
+import RouteGuard from "./components/RouteGuard";
+import axios from "axios";
+
+export const mealContext = React.createContext()
+
+axios.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      
+      config.headers['Authorization'] = 'Bearer ' + token
+    }
+    return config
+  },
+  error => {
+    Promise.reject(error)
+  }
+)
+
+
+
+function App() {
+
+  const [mealPlan, setMealPlan] = useState([])
+
+  async function addMeal(name, img, calories, protein, fat, carb, url, type) {
+    await axios.post("http://localhost:5000/diet/add-diet", { name, img, calories, protein, carb, fat, url, type }).then(res => {
+      console.log(res);
+      // url kya hai
+      setMealPlan([...mealPlan, { name, img, calories, protein, carb, fat, url, type, _id: res.data.data.dietDetails._id }]);
+    })
+  }
+
+  async function removeMeal(name) {
+    const userId = JSON.parse(localStorage.getItem('userDetails'))._id
+    // console.log(_id)
+    await axios.delete(`http://localhost:5000/diet/delete-diet`, {data: {name}} ).then(res => {
+    if(!res.data.error) 
+      setMealPlan(mealPlan.filter(meal => meal.name !== name))
+    }).catch(err => alert(err))
+  }
+
+  return (
+    <mealContext.Provider value={{ addMeal, mealPlan, removeMeal }} >
+      <BrowserRouter>
+        <Routes>
+
+    <Route path="/"  element={<SharedLayout />}>
+        <Route index element={<RouteGuard Component={Homepage} />} />
+        <Route path="/calculate" element={<RouteGuard Component={Calculate} />} />
+
+      <Route path="/blogs" element={<RouteGuard Component={Blogs} />}  />
+      
+      
+      <Route path="/calculate/breakfast" element={<RouteGuard Component={Breakfast} />} />
+
+      <Route path="/calculate/lunch" element={<RouteGuard Component={Lunch} />} />
+      
+      <Route path="/calculate/snacks" element={<RouteGuard Component={Snacks} />} />
+
+      <Route path="/calculate/dinner" element={<RouteGuard Component={Dinner} />} />
+
+    </Route>
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="*" element={<Error />} />
+
+
+        </Routes>
+      </BrowserRouter>
+    </mealContext.Provider>
+  )
+}
+
+export default App
+
---
+++ frontend/src/pages/Blogs/Blogs.jsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const blogs = () => {
+  return (
+    <div className='top-row flex flex-row ml-7 h-screen '>
+        <div className='group mt-3 h-80 w-80 ml-4'>
+              <img
+              alt="Lava"
+              src="https://images.unsplash.com/photo-1511688878353-3a2f5be94cd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aGVhbHRofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+              className="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+              />
+            <div className="p-4">
+            <Link>
+              <h3 className="text-lg font-medium text-gray-900">
+              Healthy Food Habits
+              </h3>
+            </Link>
+
+            <p className="mt-2 text-sm leading-relaxed text-gray-500 line-clamp-3">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+              dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
+              sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
+              voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
+              Molestias explicabo corporis voluptatem?
+            </p>
+          </div>
+          
+       </div>
+
+       <div className='group mt-3 h-80 w-80 ml-4'>
+                <img
+              alt="Lava"
+              src="https://images.unsplash.com/photo-1505576399279-565b52d4ac71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aGVhbHRofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1200&q=60"
+              className="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+              />
+            <div className="p-4">
+            <Link>
+              <h3 className="text-lg font-medium text-gray-900">
+              The secret to optimal health
+              </h3>
+            </Link>
+
+            <p className="mt-2 text-sm leading-relaxed text-gray-500 line-clamp-3">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+              dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
+              sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
+              voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
+              Molestias explicabo corporis voluptatem?
+            </p>
+          </div>
+          
+       </div>
+
+       <div className='group mt-3 h-80 w-80 ml-4'>
+                <img
+              alt="Lava"
+              src="https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fGhlYWx0aHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1200&q=60"
+              className="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+              />
+            <div className="p-4">
+            <Link>
+              <h3 className="text-lg font-medium text-gray-900">
+              Fuel your body, fuel your life
+              </h3>
+            </Link>
+
+            <p className="mt-2 text-sm leading-relaxed text-gray-500 line-clamp-3">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+              dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
+              sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
+              voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
+              Molestias explicabo corporis voluptatem?
+            </p>
+          </div>
+          
+       </div>
+
+       <div className='group mt-3 h-80 w-80 ml-4'>
+                <img
+              alt="Lava"
+              src="https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGhlYWx0aHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1200&q=60"
+              className="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+              />
+            <div className="p-4">
+            <Link>
+              <h3 className="text-lg font-medium text-gray-900">
+              Nourish your mind, body, and soul with a healthy diet
+              </h3>
+            </Link>
+
+            <p className="mt-2 text-sm leading-relaxed text-gray-500 line-clamp-3">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+              dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
+              sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
+              voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
+              Molestias explicabo corporis voluptatem?
+            </p>
+          </div>
+    
+          
+          
+       </div>
+    </div>
+
+
+
+    
+    
+            
+
+        
+    
+  )
+}
+
+export default blogs
